df1c05671df48f75152fab22d19561ccc9fa3e2e
sneha27042005 <sneha2005427@gmail.com>
2025-06-10 14:09:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO WORLD!!SNEHA
+          HELLO WORLD!!SNEHA 
+          I AM REACT NODE JS
         </p>
+        <button>
+          clickme
+          </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
